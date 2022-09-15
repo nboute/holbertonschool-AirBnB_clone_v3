e32bddafa85f8896acff162323703c2f492a1d16
LucileSD <4316@holbertonschool.com>
2022-09-15 11:38:22 +0000

    blueprint and json status

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""index file"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route("/status", methods = ['GET'], strict_slashes=False)
+def jsonStatus():
+    """json status"""
+    return jsonify({"status": "OK"})
