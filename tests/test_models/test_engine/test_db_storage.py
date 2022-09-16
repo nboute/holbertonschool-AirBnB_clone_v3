8d3b760e80dd5540a63ac5b72465f7a6d9358c08
LucileSD <4316@holbertonschool.com>
2022-09-16 11:23:45 +0000

    tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,29 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_method_no_args(self):
+        """test with No cls and no id"""
+        state = models.storage.get(None, None)
+        self.assertEqual(None, state)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_method_no_id(self):
+        """test with no valid id"""
+        state = models.storage.get(State, "rqhqerhq454543")
+        self.assertEqual(None, state)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count_method_all(self):
+        """test count all objects"""
+        all = models.storage.all()
+        count_all = models.storage.count()
+        self.assertEqual(count_all, len(all))
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count_method_state(self):
+        """test count for state class"""
+        all_state = models.storage.all(State)
+        count_state = models.storage.count(State)
+        self.assertEqual(count_state, len(all_state))
