09faafe9920f4509168fd2be28121863a216e60c
Nicolas Boute <4249@holbertonschool.com>
2022-09-16 14:04:16 +0200

    the checker is really weird today

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -114,15 +114,21 @@ class TestFileStorage(unittest.TestCase):
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
 
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+
     def test_get_method_no_args(self):
         """test with No cls and no id"""
         state = models.storage.get(None, None)
         self.assertEqual(None, state)
 
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+
     def test_get_method_no_id(self):
         """test with no valid id"""
         state = models.storage.get(State, "rqhqerhq454543")
         self.assertEqual(None, state)
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
 
     def test_get_method_no_id(self):
         """test with no valid id"""
@@ -130,6 +136,8 @@ class TestFileStorage(unittest.TestCase):
         for state in states.values():
             test_state = models.storage.get(State, state.id)
             self.assertEqual(state.name, test_state.name)
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
 
     def test_count_method_all(self):
         """test count all object in a class"""
